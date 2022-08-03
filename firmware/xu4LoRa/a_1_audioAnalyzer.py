0b636333452b24d4361929920a554fb121b1cf10
lakithaomal <lhw150030@utdallas.edu>
2022-08-03 13:37:39 -0500

    daily Updates

--- firmware/xu4LoRa/a_1_audioAnalyzer.py
+++ firmware/xu4LoRa/a_1_audioAnalyzer.py
@@ -53,7 +53,7 @@ def main(cfg):
                 # print(soundClassData)
 
                 baseDateTime = folderIn.split('/')
-                dateTime  = datetime.strptime( baseDateTime[-1], '%Y_%m_/%d_%H_%M_%S_%f')
+                dateTime  = datetime.datetime.strptime( baseDateTime[-1], '%Y_%m_/%d_%H_%M_%S_%f')
                 print(dateTime)
                 # Get Date Time From the File
                 # Save it as .json with proper time for its name 
