4008e4fa6114045084a85045c943ead3736b806c
lakithaomal <lhw150030@utdallas.edu>
2022-08-03 14:07:48 -0500

    daily Updates

--- firmware/xu4LoRa/a_1_audioAnalyzer.py
+++ firmware/xu4LoRa/a_1_audioAnalyzer.py
@@ -41,19 +41,16 @@ currentIndex = 0
 
 def main(cfg):
     labels = pd.read_csv("mintsAudio/labels/labels.csv") 
-    
 
     while True:
         # try:
             audioFolders = glob(tmpFolderName+ "/*/", recursive = True)
             time.sleep(1)
-            print(audioFolders)
             for folderIn in audioFolders:
                 freeze_support()
                 cfg = fn.configSetUp(cfg,folderIn,minConfidence,numOfThreads)
                 soundClassData = pd.read_csv(folderIn + '/'+ audioFileNamePre+  '.BirdNET.results.csv')
                 soundClassData["Labels"] = soundClassData["Scientific name"].map(labels.set_index("Scientific name")["Labels"])
-
                 baseDateTime = folderIn.split('/')
                 dateTimeBase  = datetime.datetime.strptime(\
                                 baseDateTime[-2], '%Y_%m_%d_%H_%M_%S_%f')
@@ -69,36 +66,6 @@ def main(cfg):
                     with open(fn.getJsonFileName(folderIn,dateTimeCurrent), "w") as outfile:
                         json.dump(sensorDictionary, outfile)
            
-            # Get Date Time From the File
-                # Save it as .json with proper time for its name 
-                # The json files should be under mintsData/jsonAudio/dateTimeFileName
-                # delete the folder 
-            
-                
-            
-            #     mSR.sensorFinisher(dateTime,"MBC001",sensorDictionary)    
-
-            # Read All the folder names 
-
-
-            # Freeze support for excecutable
-            # freeze_support()
-            # cfg = fn.configSetUp(cfg,tmpFolderName,minConfidence,numOfThreads)
-            # soundClassData = pd.read_csv(tmpFolderName + '/'+ audioFileNamePre+  '.BirdNET.results.csv')
-            # soundClassData["Labels"] = soundClassData["Scientific name"].map(labels.set_index("Scientific name")["Labels"])
-            # print(soundClassData)
-            # for index, row in soundClassData.iterrows():
-            #     sensorDictionary = OrderedDict([
-            #         ("dateTime"     ,str(dateTime + datetime.timedelta(seconds = row['Start (s)']))),
-            #         ("label"        ,row['Labels']),
-            #         ("confidence"   ,row['Confidence'])
-            #          ])
-            #     mSR.sensorFinisher(dateTime,"MBC001",sensorDictionary)
-            print("=============")            
-         
-        # except OSError as e:
-        #     print ("Error: %s - %s." % (e.filename, e.strerror))
-        #     print("Microphone Not Connected: Check connection")
 
                
 
