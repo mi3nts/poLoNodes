f5454d531f8b7e5729bc50d16b8465b48d7aa817
lakithaomal <lhw150030@utdallas.edu>
2022-08-06 16:33:25 -0500

    daily Updates

--- firmware/xu4LoRa/mintsXU4/mintsLoRaSensing.py
+++ firmware/xu4LoRa/mintsXU4/mintsLoRaSensing.py
@@ -166,7 +166,7 @@ def sensingGPGGA(dataIn,transmitReceive):
                 np.double(getLatitudeCords(dataIn.lat,dataIn.lat_dir)).tobytes().hex().zfill(16)+ \
                 np.double(getLatitudeCords(dataIn.lat,dataIn.lat_dir)).tobytes().hex().zfill(16) + \
                 np.ubyte(dataIn.gps_qual).tobytes().hex().zfill(2)+ \
-                np.ubyte(dataIn.num_of_sat).tobytes().hex().zfill(2)+ \
+                np.ubyte(dataIn.num_sats).tobytes().hex().zfill(2)+ \
                 np.float32(dataIn.horizontal_dil).tobytes().hex().zfill(8) +\
                 np.float32(dataIn.altitude).tobytes().hex().zfill(8) +\
                 np.float32(dataIn.geo_sep).tobytes().hex().zfill(8) ;
