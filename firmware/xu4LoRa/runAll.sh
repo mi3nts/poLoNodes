ff3b05b5711b500569ace416260f5b3bba3b3918
lakithaomal <lhw150030@utdallas.edu>
2022-08-08 17:12:10 +0000

    cron added

--- firmware/xu4LoRa/runAll.sh
+++ firmware/xu4LoRa/runAll.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 sleep 40
 
+kill $(pgrep -f 'python3 a_1_audioRecorder.py')
+sleep 5
+python3 a_1_audioRecorder.py &
+sleep 5
 
 kill $(pgrep -f 'python3 l_1_loRaSend.py')
 sleep 5
-python3 l_1_loRaSend.py 0
+python3 l_1_loRaSend.py &
+sleep 5
+
+kill $(pgrep -f 'a_2_audioAnalyzer.py')
+sleep 5
+python3 a_2_audioAnalyzer.py &
+sleep 5
 
